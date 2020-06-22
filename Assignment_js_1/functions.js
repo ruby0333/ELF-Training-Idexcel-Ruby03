5240492b5f2c9db68ed35edd9e619ee1f75a945a
Ruby <ruby.das0395@gmail.com>
2020-06-22 18:57:04 +0530

    Assignment

---
+++ Assignment_js_1/functions.js
@@ -0,0 +1,54 @@
+console.log("====>Named Function<====");
+
+function myfunction(a,b,c){
+ 
+return a*b*c;
+
+}
+console.log(myfunction(1,6,7));
+
+
+console.log("===>Function Expression<===");
+
+var getRectArea = function(width, height) {
+
+return width * height;
+
+
+};
+
+var res = getRectArea(6,7)
+
+console.log(res);
+
+
+console.log("===>IIFE<==="); 
+function Greet()
+{
+ console.log("welcome to javaScript");
+
+(function() {
+console.log("welcome to java Script"); })();
+console.log("hello world!");
+}
+console.log(Greet());
+
+console.log("===>Arrow Function<===");
+var printValue = (a) => {
+ console.log("value =" ,a);
+}
+
+printValue(5)
+
+console.log("----------------------------------------");
+var a=[2,7,5]
+var b=[23,65,10]
+function printArray(arr){
+console.log(arr);
+}
+var person={
+ name='Priya',
+ age=19
+}
+
+printArray(person)
